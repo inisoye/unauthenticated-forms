7bd2279fa9770ed801a43062a1bb4d20e14eef8e
Inioluwa Akinyosoye <inisoye@gmail.com>
2021-09-10 17:06:21 +0100

    Add updates

--- src/components/FormPageHeader.js
+++ src/components/FormPageHeader.js
@@ -6,7 +6,7 @@ import AvatarButton from '../components/AvatarButton';
 
 // Styles
 const StyledPageHeaderWrapper = tw.header`px-6 md:px-20 pt-3 pb-3.5 md:py-4 bg-chevyTeal-light bg-opacity-50 md:bg-opacity-60 border-b border-chevyBlue-dark border-opacity-10 md:flex md:justify-between`;
-const StyledPageHeading = tw.h1`text-2xl md:text-3xl tracking-tighter md:pt-12`;
+const StyledPageHeading = tw.h1`text-2xl tracking-tighter md:pt-12`;
 
 function FormPageHeader({ formName }) {
   const isBelowMedium = useMediaQuery({ minWidth: 0, maxWidth: 768 });
